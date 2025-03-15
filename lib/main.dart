1e1976bc004aa419ba88a97959cf8fd0dda5dac8
Fabio Nunes <fabio.pedro@live.com>
2025-03-15 19:14:00 -0300

    add packge http

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app_web_api/services/journal_service.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'screens/home_screen/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
+
+  JournalService service = JournalService();
+  service.register("Ola Mundo");
+  service.get();
 }
 
 class MyApp extends StatelessWidget {
